61411c0db63704455822046da0164a4558f1b68f
brendan-kelly <brendanhkelly@gmail.com>
2015-09-26 15:42:49 -0700

    Delete credentials.js

--- credentials.js
+++
@@ -1,8 +0,0 @@
-var credentials = {
-  consumer_key: 's0h9LewIoGk5Jcv56iUSF79DC',
-  consumer_secret: 'x944xC94e5FlSS1tFOQJ5qyhZWdRqiUq2TWPYvM356bseFntm5',
-  access_token_key: '2989504741-mY4Rb2Ku5xYZjwF3ECqLHFNBHtDX2hzz9wPH97u',
-  access_token_secret: 'X6KYUfYFU6T81toUdyf0wJOlC6goTPe3oKR27FIiOFIH3',
-};
- 
-module.exports = credentials;
